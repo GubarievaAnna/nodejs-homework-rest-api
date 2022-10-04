8652e6782d02132305dc3e9a9581cce3ef93b86c
GubarievaAnna <annagubarieva@gmail.com>
2022-10-04 19:21:57 +0300

    updateTitle

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 
-const contactsRouter = require("./controller");
+const contactsRouter = require("./routes/contactsRouter");
 
 const app = express();
 
