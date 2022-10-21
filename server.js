786dc098de20d56501b5d178ca93f4e69599e25c
GubarievaAnna <annagubarieva@gmail.com>
2022-10-21 16:34:36 +0300

    done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Database connection successful");
 
-    app.listen(3001, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server running. Use our API on port: 3000");
     });
   })
